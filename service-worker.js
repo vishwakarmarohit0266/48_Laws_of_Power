7b0674817405b0fddda7bf17fc163f69b7ca66a9
Rohit Vishwakarma <vishwakarmarohit0266@gmail.com>
2026-05-28 14:44:51 +0530

    Enhance service worker for offline support
    Updated service worker to cache offline.html and handle fetch failures.

--- service-worker.js
+++ service-worker.js
@@ -5,21 +5,82 @@ const urlsToCache = [
   "./index.html",
   "./main.css",
   "./my.js",
-  "./manifest.json"
+  "./offline.html"
+
 ];
 
-// Install
+/* Install */
+
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+
+    caches.open(CACHE_NAME).then(cache => {
+
+      return cache.addAll(FILES_TO_CACHE);
+
+    })
+
   );
+
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+/* Activate */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
   );
+
+});
+
+/* Fetch */
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+    .then(response => {
+
+      return response;
+
+    })
+
+    .catch(() => {
+
+      return caches.match(event.request)
+
+      .then(response => {
+
+        return response || caches.match("./offline.html");
+
+      });
+
+    })
+
+  );
+
 });
